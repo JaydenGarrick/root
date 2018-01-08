961165ecc9e7be67d3421d32be18826120b2b6d4
JaydenGarrick <33009669+JaydenGarrick@users.noreply.github.com>
2018-01-08 16:01:31 -0700

    Add Xcode project to repo

---
+++ Root/Root/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Root
+//
+//  Created by Jayden Garrick on 1/8/18.
+//  Copyright © 2018 Jayden Garrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
