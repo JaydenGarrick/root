87b225bcce43605524383f4e356ed18cd52a4568
fmartin0212 <fmartin0212@gmail.com>
2018-01-09 15:33:45 -0700

    add initial model setup

--- Root/Root/Models/Comment.swift
+++ Root/Root/Models/Comment.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+import CloudKit
+
+class Comment {
+    let text: String
+    var timestamp: Date? = nil
+    let creatorID: CKReference
+    let eventID: CKReference
+    
+    var cloudKitRecordID: CKRecordID?
+    
+    init(text: String, creatorID: CKReference, eventID: CKReference) {
+        self.text = text
+        self.creatorID = creatorID
+        self.eventID = eventID
+        
+    }
+    
+    // CloudKit
+    // Turn the CKRecord that we receive from the server to a Comment object.
+    init?(ckRecord: CKRecord) {
+        
+        guard let text = ckRecord["text"] as? String,
+            let creatorID = ckRecord["creatorID"] as? CKReference,
+            let eventID = ckRecord["eventID"] as? CKReference
+            else { return nil }
+        
+        self.text = text
+        self.creatorID = creatorID
+        self.eventID = eventID
+        self.timestamp = ckRecord.creationDate
+        
+    }
+    
+}
+
+// Turn the Comment model object into a CKRecrod before giving it to Apple's servers
+extension CKRecord {
+    
+    convenience init(comment: Comment) {
+        
+        let recordID = comment.cloudKitRecordID ?? CKRecordID(recordName: UUID().uuidString)
+        
+        self.init(recordType: "Comment", recordID: recordID)
+        
+        self.setValue(comment.text, forKey: "text")
+        self.setValue(comment.creatorID, forKey: "creatorID")
+        self.setValue(comment.eventID, forKey: "eventID")
+        
+    }
+    
+}
+
--- Root/Root/Models/Event.swift
+++ Root/Root/Models/Event.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import CloudKit
+
+class Event {
+    var name: String
+    var dateAndTime: Date
+    var description: String
+    var venue: String
+    var artists: [User] = []
+    var comments: [Comment] = []
+    let creatorID: CKReference
+    
+    var ckRecordID: CKRecordID?
+    
+    init(name: String, dateAndTime: Date, description: String, venue: String, creatorID: CKReference) {
+        self.name = name
+        self.dateAndTime = dateAndTime
+        self.description = description
+        self.venue = venue
+        self.creatorID = creatorID
+    }
+    
+    // CloudKit
+    // Turn the CKRecord from Apple's servers into an Event object
+    init?(ckRecord: CKRecord) {
+        
+        guard let name = ckRecord["name"] as? String,
+            let dateAndTime = ckRecord["dateAndTime"] as? Date,
+            let description = ckRecord["description"] as? String,
+            let venue = ckRecord["venue"] as? String,
+            let creatorID = ckRecord["creatorID"] as? CKReference
+            else { return nil }
+        
+        self.name = name
+        self.dateAndTime = dateAndTime
+        self.description = description
+        self.venue = venue
+        self.creatorID = creatorID
+        
+    }
+    
+}
+
+// Turn Event model object into CKRecord before being sent to Apple's servers
+extension CKRecord {
+    
+    convenience init(event: Event) {
+        
+        let recordID = event.ckRecordID ?? CKRecordID(recordName: UUID().uuidString)
+        
+        self.init(recordType: "Event", recordID: recordID)
+        
+        self.setValue(event.name, forKey: "name")
+        self.setValue(event.dateAndTime, forKey: "dateAndTime")
+        self.setValue(event.description, forKey: "description")
+        self.setValue(event.venue, forKey: "venue")
+        self.setValue(event.creatorID, forKey: "creatorID")
+        
+    }
+    
+}
