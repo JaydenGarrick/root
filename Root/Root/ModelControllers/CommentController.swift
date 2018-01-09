029375b0a1631d55e776be864b216be1f24be5ef
JaydenGarrick <33009669+JaydenGarrick@users.noreply.github.com>
2018-01-09 10:23:21 -0700

    Add initial structure

---
+++ Root/Root/ModelControllers/CommentController.swift
@@ -0,0 +1,9 @@
+//
+//  CommentController.swift
+//  Root
+//
+//  Created by Jayden Garrick on 1/9/18.
+//  Copyright © 2018 Jayden Garrick. All rights reserved.
+//
+
+import Foundation
---
+++ Root/Root/Models/Comment.swift
@@ -0,0 +1,9 @@
+//
+//  Comment.swift
+//  Root
+//
+//  Created by Jayden Garrick on 1/9/18.
+//  Copyright © 2018 Jayden Garrick. All rights reserved.
+//
+
+import Foundation
