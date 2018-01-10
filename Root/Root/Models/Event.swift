b9f938018125bb84d31c85044c5b39ed326a1b8d
JaydenGarrick <33009669+JaydenGarrick@users.noreply.github.com>
2018-01-10 16:52:28 -0700

    Update Event model to conform to MKAnnotation. Update initializers

--- Root/Root/Models/Event.swift
+++ Root/Root/Models/Event.swift
@@ -10,7 +10,7 @@ import Foundation
 import CloudKit
 import MapKit
 
-class Event: NSObject {
+class Event: NSObject, MKAnnotation {
     var name: String
     var eventImage: Data
     var dateAndTime: Date
@@ -18,17 +18,29 @@ class Event: NSObject {
     var venue: String
     var artists: [User] = []
     var comments: [Comment] = []
-    let creatorID: CKReference
     
+    
+    // CloudKit
+    let creatorID: CKReference
     var ckRecordID: CKRecordID?
     
-    init(name: String, eventImage: Data, dateAndTime: Date, description: String, venue: String, creatorID: CKReference) {
+    // Mapkit
+    var coordinate: CLLocationCoordinate2D
+    var title: String? {
+        return name
+    }
+    var subtitle: String? {
+        return venue
+    }
+    
+    init(name: String, eventImage: Data, dateAndTime: Date, description: String, venue: String, creatorID: CKReference, coordinate: CLLocationCoordinate2D) {
         self.name = name
         self.eventImage = eventImage
         self.dateAndTime = dateAndTime
         self.eventDescription = description
         self.venue = venue
         self.creatorID = creatorID
+        self.coordinate = coordinate
     }
     
     // CloudKit
@@ -38,10 +50,12 @@ class Event: NSObject {
         guard let name = ckRecord["name"] as? String,
             let eventImage = ckRecord["eventImage"] as? Data,
             let dateAndTime = ckRecord["dateAndTime"] as? Date,
-            let description = ckRecord["description"] as? String,
+            let description = ckRecord["eventDescription"] as? String,
             let venue = ckRecord["venue"] as? String,
-            let creatorID = ckRecord["creatorID"] as? CKReference
-            else { return nil }
+            let creatorID = ckRecord["creatorID"] as? CKReference,
+            // FIXME: - Change CKRecord for saving
+            let latitude = ckRecord["latitude"] as? Double,
+            let longitude = ckRecord["longitude"] as? Double else { return nil }
         
         self.name = name
         self.eventImage = eventImage
@@ -49,36 +63,13 @@ class Event: NSObject {
         self.eventDescription = description
         self.venue = venue
         self.creatorID = creatorID
+        self.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         
     }
     
 }
 
-// Mapkit Annotation Protcol
-extension Event: MKAnnotation {
-    
-    var coordinate: CLLocationCoordinate2D {
-        let geocoder = CLGeocoder()
-        var returnedCoordinate = CLLocationCoordinate2D()
-        geocoder.geocodeAddressString(venue) { (placemarks, error) in
-            if let error = error {
-                print("Geocoder error from venue name: \(error.localizedDescription)")
-            }
-            let placemark = placemarks?.first
-            guard let coordinate = placemark?.location?.coordinate else { return }
-            returnedCoordinate = coordinate
-        }
-        return returnedCoordinate
-    }
-    
-    var title: String? {
-        return name
-    }
-    
-    var subtitle: String? {
-        return venue
-    }
-}
+
 
 // Turn Event model object into CKRecord before being sent to Apple's servers
 extension CKRecord {
@@ -89,13 +80,18 @@ extension CKRecord {
         
         self.init(recordType: "Event", recordID: recordID)
         
+        // FIXME : - Change CKRecord for saving
         self.setValue(event.name, forKey: "name")
         self.setValue(event.eventImage, forKey: "eventImage")
         self.setValue(event.dateAndTime, forKey: "dateAndTime")
-        self.setValue(event.eventDescription, forKey: "description")
+        self.setValue(event.eventDescription, forKey: "eventDescription")
         self.setValue(event.venue, forKey: "venue")
         self.setValue(event.creatorID, forKey: "creatorID")
+        self.setValue(event.coordinate.latitude, forKey: "latitude")
+        self.setValue(event.coordinate.longitude, forKey: "longitude")
         
     }
     
 }
+
+
