0a3d540709ec3048d94474e3416b8520be5a0af1
JaydenGarrick <33009669+JaydenGarrick@users.noreply.github.com>
2018-01-11 15:20:46 -0700

    Add event detail sb Also add extension to VC for keyboard

---
+++ Root/Root/Resources/extensions.swift
@@ -0,0 +1,21 @@
+//
+//  extensions.swift
+//  Root
+//
+//  Created by Jayden Garrick on 1/11/18.
+//  Copyright © 2018 Jayden Garrick. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
