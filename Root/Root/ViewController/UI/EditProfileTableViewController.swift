b07630b24ee5200450316fb2df5a922f76ad8e0f
Edmund <bollenbacher.edmund@gmail.com>
2018-01-17 17:20:22 -0700

    fix EditProfile image

--- Root/Root/ViewController/UI/EditProfileTableViewController.swift
+++ Root/Root/ViewController/UI/EditProfileTableViewController.swift
@@ -153,11 +153,12 @@ class EditProfileTableViewController: UITableViewController, UIImagePickerContro
         pickerController.sourceType = .photoLibrary
         pickerController.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
         
-        guard let profilePicture = info[UIImagePickerControllerOriginalImage] as? UIImage,
-        let chosenImage = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        guard let profilePicture = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        // let chosenImage = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        
         //        userProfilePictureImageView.contentMode = .scaleAspectFit
-        userProfilePictureImageView.image = chosenImage
         userProfilePictureImageView.image = profilePicture
+        //userProfilePictureImageView.image = profilePicture
         
         DispatchQueue.main.async {
             
