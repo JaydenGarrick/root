c39ae9e47d8a23a838620268c3166c1bdc3278b0
Edmund <bollenbacher.edmund@gmail.com>
2018-01-21 16:21:37 -0700

    add contrainsts EventDetail.storyboard

--- Root/Root/Views/EventTableViewCell.swift
+++ Root/Root/Views/EventTableViewCell.swift
@@ -24,15 +24,15 @@ class EventTableViewCell: UITableViewCell {
     
     weak var delegate: EventTableViewCellDelegate?
     
-    override func awakeFromNib() {
-        backgroundShadowView.backgroundColor = .white
-        contentView.backgroundColor = UIColor(red: 240/255.0, green: 240/255.0, blue: 240/255.0, alpha: 1.0)
-        backgroundShadowView.layer.cornerRadius = 3.0
-        backgroundShadowView.layer.masksToBounds = false
-        backgroundShadowView.layer.shadowColor = UIColor.black.withAlphaComponent(0.5).cgColor
-        backgroundShadowView.layer.shadowOffset = CGSize(width: 0, height: 0)
-        backgroundShadowView.layer.shadowOpacity = 0.8
-        
-    }
+//    override func awakeFromNib() {
+//        backgroundShadowView.backgroundColor = .white
+//        contentView.backgroundColor = UIColor(red: 240/255.0, green: 240/255.0, blue: 240/255.0, alpha: 1.0)
+//        backgroundShadowView.layer.cornerRadius = 3.0
+//        backgroundShadowView.layer.masksToBounds = false
+//        backgroundShadowView.layer.shadowColor = UIColor.black.withAlphaComponent(0.5).cgColor
+//        backgroundShadowView.layer.shadowOffset = CGSize(width: 0, height: 0)
+//        backgroundShadowView.layer.shadowOpacity = 0.8
+//
+//    }
 
 }
