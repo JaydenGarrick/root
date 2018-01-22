1ef9d42918cb5cf0ba99cfe6c2bea21f3532d217
JaydenGarrick <33009669+JaydenGarrick@users.noreply.github.com>
2018-01-22 10:40:43 -0700

    Add tab bar images

--- Root/Root/Views/CommentTableViewCell.swift
+++ Root/Root/Views/CommentTableViewCell.swift
@@ -27,6 +27,7 @@ class CommentTableViewCell: UITableViewCell {
     
     @IBOutlet weak var commentCreatorProfileImageView: UIImageView!
     @IBOutlet weak var commentTextLabel: UILabel!
+    @IBOutlet weak var backgroundShadowView: CommentTableViewCell!
     
     func updateViews(_ commentCreator: User) {
         guard let commentCreatorProfilePictureAsData = commentCreator.profilePicture,
@@ -37,6 +38,17 @@ class CommentTableViewCell: UITableViewCell {
         commentCreatorProfileImageView.image = commentProfilePicture
         commentTextLabel.text = comment.text
         
+        
+    }
+    
+    override func awakeFromNib() {
+        // backgroundShadowView.backgroundColor = .whi
+        // contentView.backgroundColor = UIColor(red: 240/255.0, green: 240/255.0, blue: 240/255.0, alpha: 1.0)
+        backgroundShadowView.layer.cornerRadius = 3.0
+        backgroundShadowView.layer.masksToBounds = false
+        backgroundShadowView.layer.shadowColor = UIColor.black.withAlphaComponent(0.5).cgColor
+        backgroundShadowView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        backgroundShadowView.layer.shadowOpacity = 0.8
         
     }
 }
